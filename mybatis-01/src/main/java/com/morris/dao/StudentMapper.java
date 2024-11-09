11fc5e625d73d2e3b8fac61c05696d32740abdc5
morris <morris91re@gmail.com>
2024-11-09 16:57:40 +0800

    feat:多位學生關聯一位導師 (多對一)

--- mybatis-01/src/main/java/com/morris/dao/StudentMapper.java
+++ mybatis-01/src/main/java/com/morris/dao/StudentMapper.java
@@ -1,4 +1,10 @@
 package com.morris.dao;
 
+import com.morris.pojo.Student;
+
+import java.util.List;
+
 public interface StudentMapper {
+    // 查詢所有的學生訊息，以及對應的老師信息
+    public List<Student> getStudent();
 }
--- mybatis-01/src/test/java/com/morris/dao/MyTest.java
+++ mybatis-01/src/test/java/com/morris/dao/MyTest.java
@@ -1,9 +1,12 @@
 package com.morris.dao;
 
+import com.morris.pojo.Student;
 import com.morris.pojo.Teacher;
 import com.morris.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
+
+import java.util.List;
 
 public class MyTest {
 
@@ -18,6 +21,25 @@ public class MyTest {
             Teacher teacher = teacherMapper.getTeacher(1);
             System.out.println(teacher);
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void getStudent(){
+
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        try {
+            StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+
+            List<Student> studentList = studentMapper.getStudent();
+            for (Student student : studentList) {
+                System.out.println(student);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
